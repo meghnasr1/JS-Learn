78316bef93a78f0e4d29eb8cdc54f54f5c4b2249
meghnasr1 <130906058+meghnasr1@users.noreply.github.com>
2026-01-05 07:07:24 +0000

    Added 2 new files

---
+++ o1-basic/01_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treate all the JS code as per newer version
+//alert(3+3)
+let name = "Meghna"
+let age = 23
+let isLoggedIn = false
+ // number =>2 to pwer 53
+ //bigint => larger than 2 to power 53
+ //string =>"Meghna"
+ //boolean => true/false
+ //null => standalone value
+ //undefined => variable not assigned any value
+ //object => key value pairs
+ //symbol => unique
+ console.log(typeof "hitesh");
+ console.log(typeof age); 
+ console.log(typeof null);
+ 
---
+++ o1-basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "rajputmsr8@gmail.com"
+var accountPassword = "R@jput1234"
+accountcity = "Indore"
+
+//accountId = 2
+accountEmail = "meghna@gmail.com"
+accountPassword = "Meghna@1234"
+accountcity = "Bhopal"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountcity])
