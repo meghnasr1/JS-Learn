d156c474d4316d45f44ee24fab3923b679a428b0
meghnasr1 <130906058+meghnasr1@users.noreply.github.com>
2026-01-06 05:20:31 +0000

    Conversion file added

---
+++ o1-basic/01_conversions.js
@@ -0,0 +1,8 @@
+let score = "33abc";
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let ValueInString = Number(score);
+console.log(ValueInString);
+console.log(typeof ValueInString);
